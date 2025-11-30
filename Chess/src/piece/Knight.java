b3f7480feacde61bb829b6cd550a0a1677df9651
NTKhoa <siper.chicken@gmail.com>
2025-11-30 19:59:16 +0700

    Revert "feature: adding front menu interface and display the captured pieces"

--- Chess/src/piece/Knight.java
+++ Chess/src/piece/Knight.java
@@ -15,16 +15,14 @@ public class Knight extends piece {
             image = getImage("/pieceImage/bknight");
         }
     }
-
-    public boolean canMove(int targetCol, int targetRow){
-        if(isWithinboard(targetCol, targetRow)){
-            if((Math.abs(targetCol-preCOL) * Math.abs(targetRow - preROW)) == 2){
-                if(isvalidSquare(targetCol, targetRow)){
-                    return true;
+    public boolean canMove(int targetCol,int targetRow){
+        if(isWithinboard(targetCol,targetRow)){
+                if(isvalidSquare(targetCol,targetRow)) {
+                    if(Math.abs(targetCol-preCOL)*Math.abs(targetRow-preROW)==2){
+                        return true;
                 }
             }
         }
         return false;
     }
-    
 }
